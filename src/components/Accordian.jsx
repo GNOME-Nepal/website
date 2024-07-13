47a76a113ab6b42b5b56557002e723f1f2e97fdf
Manjul Tamrakar <manjultamrakar4@gmail.com>
2024-07-13 19:02:56 +0545

    feat: started work on the FAQ Section
    - made an accordian component for better reusability
    - added an assets folder along with an arrowicon asset
    - added clsx dependency for better class management
    - implemented mobile ui for FAQ

---
+++ src/components/Accordian.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import clsx from 'clsx';
+import ArrowIcon from '../assets/ArrowIcon';
+
+const Accordian = ({ items, className, ...props }) => {
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  const handleToggle = (index) => {
+    setActiveIndex(activeIndex === index ? null : index);
+  };
+
+  return (
+    <div className={clsx('w-full max-w-md mx-auto border border-[#d9d9d9] rounded-md', className)}>
+      {items.map((item, index) => (
+        <div key={index} className="">
+          <button
+            className="w-full p-4 text-left"
+            onClick={() => handleToggle(index)}
+          >
+            <div className="flex justify-between items-center">
+              <div className="flex flex-row gap-2">
+                <div className={clsx('w-6 h-6 flex-shrink-0 rounded-full', {
+                  'bg-[purple]': index === activeIndex,
+                  'bg-[#D19DE2]': index !== activeIndex,
+                })} />
+                <span className="text-lg font-medium text-gray-900">{item.question}</span>
+              </div>
+              <span className={clsx('text-lg font-medium', {
+                'text-[purple]': index === activeIndex,
+                'text-[#D19DE2]': index !== activeIndex,
+              })}>
+                <ArrowIcon className={clsx({
+                  'rotate-180': index === activeIndex,
+                  'rotate-90': index !== activeIndex,
+                })} />
+              </span>
+            </div>
+          </button>
+          {activeIndex === index && (
+            <div className="p-4 bg-gray-50">
+              <p className="text-gray-700">{item.answer}</p>
+            </div>
+          )}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Accordian;
--- src/sections/FAQ.jsx
+++ src/sections/FAQ.jsx
@@ -1,10 +1,39 @@
 import React from "react";
+import Accordian from "../components/Accordian";
+
+const items = [
+  {
+    question: 'What is Gnome Nepal',
+    answer: 'Gnome Nepal is the open source organization whose main motive is to spread collaboration and open source in Nepal.',
+  },
+  {
+    question: 'How we can join this Community?',
+    answer: 'Join the community with our discord!!',
+  },
+  {
+    question: 'What is Opensource?',
+    answer: 'Open-source software is computer software that is released under a license in which the copyright holder grants users the rights to use, study, change, and distribute the software and its source code to anyone and for any purpose',
+  },
+  {
+    question: 'Can I Contribute to the org?',
+    answer: 'Yes, You can!!',
+  },
+  {
+    question: 'Is there are exciting event?',
+    answer: 'Check out our upcoming events in the events sections.',
+  },
+];
+
 
 const FAQ = () => {
   return (
     <>
-      <section id="faq" className="min-h-[100vh] flex justify-center items-center border-2">
-        FAQ
+      <section id="faq" className="min-h-[100vh] border-2 w-full h-full items-center justify-center flex flex-col sm:items-start px-4 sm:px-12 py-12">
+        <div className="text-[purple] font-medium rounded-[50px] text-center py-2 p-4 bg-[#e7cdf0] border-r-[50%]">Something you are wondering</div>
+        <h1 className="text-2xl font-bold my-2 hidden sm:block">Frequently Asked Questions</h1>
+        <h1 className="text-2xl font-bold my-2 block sm:hidden">FAQs</h1>
+
+        <Accordian items={items} className="block sm:hidden mt-8" />
       </section>
     </>
   );
