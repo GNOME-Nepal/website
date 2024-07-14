1a80ccbb4fa22427f2ab046345cff503719f06eb
notashwinii <ashwinii.subedi@gmail.com>
2024-07-14 13:27:27 +0545

    footer updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,14 +2,20 @@ import React from "react";
 
 const Footer = () => {
   return (
+    <footer className="relative w-full">
     <div className="bg-gray-900 text-white p-6 flex flex-col items-center space-y-6">
-    <div className="bg-gradient-to-r from-teal-400 to-blue-500 p-4 rounded-lg flex flex-col items-center space-y-4">
-      <h2 className="text-xl font-semibold">Become a Sponsor?</h2>
-      <button className="bg-white text-blue-500 px-4 py-2 rounded-md">
-          Click here
-      </button>
-  </div>
-</div>);
+            <div className="absolute bottom-[50%] left-1/2 transform -translate-x-1/2 z-10 bg-gradient-to-r from-teal-400 to-blue-500 p-4 rounded-lg flex flex-col items-center space-y-4">
+            <h2 className="text-xl font-semibold">Become a Sponsor?</h2>
+            <button className="bg-white text-blue-500 px-4 py-2 rounded-md">
+                Click here
+            </button>
+        </div>
+    </div>
+    
+    <div className="relative w-full h-40 bg-[#241F31] flex items-center justify-between p-6">
+        
+    </div>
+</footer>);
 };
 
 export default Footer;
