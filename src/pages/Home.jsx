d75388cfe2d14aa36c2c6e258547b3bde3f3e3d4
utsavbhattarai007 <ninja08469@gmail.com>
2024-07-16 23:20:49 +0545

    fix: merge conflits along with design bugs

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
     <>
       <Hero />
 
-      <div className="container mx-auto py-8">
+      <div className="container mx-auto py-8 flex flex-col gap-8">
         <section id="aboutus" className="py-8">
           <Aboutus />
         </section>
--- src/sections/Contributors.jsx
+++ src/sections/Contributors.jsx
@@ -1,21 +1,25 @@
-import React from 'react';
-import ContributorsDetails from '../contribution/con';
+import React from "react";
+import ContributorsDetails from "../components/ContributorsDetails";
 
-
-
-function Contributors () {
+function Contributors() {
   return (
     <>
-      <section id="contributors" className="min-h-[100vh] flex flex-col items-center border-2">
-        <header className= "w-full mt-60 ml-60  ">
-       <p className="bg-green-200 w-60 rounded-lg pl-5 text-green-600">People behind the magic</p>
-       <br />
-       <h1 className="text-3xl font-bold">Contributors</h1>
-       </header>
-      <ContributorsDetails /> 
+      <section
+        id="contributors"
+        className="min-h-content flex flex-col items-center"
+      >
+        <header className="w-full flex flex-col items-center md:items-start">
+          <div className="text-green-normal font-medium rounded-[50px] text-center py-2 p-4 bg-green-light border-r-[50%] w-60">
+            People behind the magic
+          </div>
+          <h1 className="text-3xl md:text-4xl font-bold my-[8px] lg:block">
+            Contributors
+          </h1>
+        </header>
+        <ContributorsDetails />
       </section>
     </>
   );
-};
+}
 
 export default Contributors;
