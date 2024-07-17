556baf366ad69ff0c047911215c01916b38c68f1
notashwinii <ashwinii.subedi@gmail.com>
2024-07-17 12:56:59 +0545

    buttons fixed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,19 +6,24 @@ const Footer = () => {
 
       <div className="bg-gray-900 text-white p-6 flex flex-col items-center space-y-6">
 
-        <div className="absolute bottom-[50%] left-1/2 transform -translate-x-1/2 z-10 bg-gradient-to-r from-teal-400 to-blue-500 p-4 rounded-lg flex justify-center items-center space-y-8 w-[70vw] h-[150px] mx-auto">
+        <div className="absolute bottom-[60%] left-1/2 transform -translate-x-1/2 z-10 bg-gradient-to-r from-teal-400 to-blue-500 p-4 rounded-lg flex justify-center items-center space-y-8 w-[70vw] h-[150px] mx-auto">
 
           <div className="flex items-center space-x-[20vw]">
 
             <h2 className="text-4xl font-bold">Become a Sponsor?</h2>
-            <button className="bg-white text-black px-4 py-2 rounded-lg">
+            <button className="bg-white text-black w-48 h-10 rounded-full">
               Click here
             </button>
 
           </div>
         </div>
       </div>
-      <div className="relative w-full h-40 bg-[#241F31] flex items-center justify-between p-6">
+      <div className="relative w-full h-40 bg-[#241F31] flex justify-end items-center p-6 ">
+
+        <div>
+          <button ></button>
+        </div>
+
 <div className="space-x-5 ">
   <button className="bg-white w-10 h-10 text-black rounded-lg hover:bg-gray">
 
