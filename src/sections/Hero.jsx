85da4441076aac452ca2ce5a6d32b5a8a67c4eaf
anandacdr <jazzw815@gmail.com>
2024-07-20 06:56:08 +0545

    mobile-responsive

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import bgGnomeNepal from "../assets/imgs/bg-GnomeNepal.png";
-import homeDesign from "../assets/imgs/home-design.png"
+import homeDesign from "../assets/imgs/home-design.png";
+import mobileViewGnomeNepal from "../assets/imgs/mobile-view-GnomeNepal.png";
 
 const Hero = () => {
   return (
@@ -8,24 +9,31 @@ const Hero = () => {
     //   A Organization which core goals is to promote open source in Nepal.
     // </section>
 
- 
-    <div className="relative hero-section h-[80vh] flex flex-col justify-between">
-      <div className="relative z-10 flex-grow">
+    <div className="relative hero-section min-h-screen flex flex-col justify-between px-4 md:px-8">
+      <div className="relative z-10 flex-grow hidden md:block transition duration-500 ease-in-out">
         <img src={bgGnomeNepal} alt="Gnome Nepal" className="w-full h-auto" />
       </div>
-      <div className="flex justify-center items-center h-screen bg-gray-100 mt-4">
+      <div className="relative z-10 flex-grow mt-0 block md:hidden transition duration-500 ease-in-out">
+        <img
+          src={mobileViewGnomeNepal}
+          alt="Gnome Nepal"
+          className="w-full h-auto"
+        />
+      </div>
+      <div className="flex justify-center items-center my-4 transition duration-500 ease-in-out">
         <a href="#aboutus">
           <button
             id="aboutus"
-            className="block py-2 px-3 hover:bg-gray md:hover:text-white rounded md:text-gray-900 md:p-0 md:hover:bg-black md:px-4 md:py-2 md:rounded-full md:bg-black md:text-white transition delay-80 text-2xl"
+            className="block py-2 px-6 rounded-full bg-black text-white transition duration-500 ease-in-out text-xl hover:bg-gray-200 hover:text-white md:hover:bg-black md:hover:text-white"
           >
             About us
           </button>
         </a>
-        
       </div>
-
-      <div id="home-design" className="relative z-10 flex-grow mt-0">
+      <div
+        id="home-design"
+        className="relative z-10 flex-grow mt-0 hidden md:block transition duration-500 ease-in-out"
+      >
         <img src={homeDesign} alt="Home Design" className="w-full h-auto" />
       </div>
     </div>
