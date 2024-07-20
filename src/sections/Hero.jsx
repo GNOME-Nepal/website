f3db88a5b582aa3b3170bb343402ae1e22c47398
supremex04 <bhandarisupreme7@gmail.com>
2024-07-20 08:27:25 +0545

    mobile responsive

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,30 +1,32 @@
 import React from "react";
 import bgGnomeNepal from "../assets/imgs/upper_section.svg";
+import bgGnomeNepalMobile from "../assets/imgs/upper_section_mobile.svg";
 import homeDesign from "../assets/imgs/bottom_section.svg";
 
 const Hero = () => {
   return (
-    // <section className="min-h-[100vh] flex justify-center items-center border-2">
-    //   A Organization which core goals is to promote open source in Nepal.
-    // </section>
-
- 
-    <div className="relative hero-section h-[80vh] flex flex-col justify-between">
+    <div className="relative hero-section h-screen flex flex-col justify-between">
       <div className="relative z-10 flex-grow">
-        <img src={bgGnomeNepal} alt="Gnome Nepal" className="w-full h-auto" />
+        {/* Desktop SVG */}
+        <img src={bgGnomeNepal} alt="Gnome Nepal" className="hidden md:block w-full h-auto" />
+        
+        {/* Mobile SVG */}
+        <div className="block md:hidden w-full h-auto flex items-center justify-center mt-32 mx-auto">
+          <img src={bgGnomeNepalMobile} alt="Gnome Nepal Mobile" className="w-full object-cover" />
+        </div>
       </div>
-      <div className="flex justify-center items-center h-screen bg-gray-100 mt-4">
+      <div className="flex justify-center items-center h-1/2 bg-gray-100 mt-4 md:mt-4 lg:mt-12">
         <a href="#aboutus">
           <button
             id="aboutus"
-            className="block py-2 px-3 hover:bg-gray md:hover:text-white rounded md:text-gray-900 md:p-0 md:hover:bg-black md:px-4 md:py-2 md:rounded-full md:bg-black md:text-white transition delay-80 text-2xl"
+            className="block py-3 px-10 bg-black text-white rounded-full transition duration-200 ease-in-out transform hover:scale-105 hover:bg-gray-700 hover:text-white shadow-md hover:shadow-lg text-xl md:text-2xl"
           >
             About us
           </button>
         </a>
       </div>
-
-      <div id="home-design" className="relative z-10 flex-grow mt-0">
+      {/* Conditionally render bottom section only on desktop */}
+      <div className="hidden md:block" id="home-design">
         <img src={homeDesign} alt="Home Design" className="w-full h-auto" />
       </div>
     </div>
