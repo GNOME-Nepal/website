51029d926cc1cdd3ffe3dec69ce8d24584106e39
utsavdotdev <95236817+utsavdotdev@users.noreply.github.com>
2024-10-02 22:43:54 +0545

    feat: Add newsletter section and drafted the previous event

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import Hero from "../sections/Hero";
-import { Aboutus, Contributors, FAQ, Events } from "../sections";
+import { Aboutus, Contributors, FAQ, Events, Newsletter } from "../sections";
 
 const Home = () => {
     return (
@@ -10,6 +10,7 @@ const Home = () => {
                 <Events />
                 <Contributors />
                 <FAQ />
+                <Newsletter />
             </div>
         </>
     );
