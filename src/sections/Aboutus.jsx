24c33125a11d04f28b19184a766dfce9457dae6d
kshetritej <tejghartikshetri@gmail.com>
2024-10-08 09:25:08 +0545

    fix(events-section): uneven spacings

--- src/sections/Aboutus.jsx
+++ src/sections/Aboutus.jsx
@@ -4,13 +4,13 @@ import { ArrowRight, ChevronRight, Shrub } from "lucide-react";
 import React from "react";
 
 const Aboutus = () => {
-    const missions = [
-                 "Promote Open-Source Technologies",
-                 "Build a Strong Community",
-                 "Organize Events and Workshops",
-                 "Engage with International Open-Source Communities",
-    ];
-    
+  const missions = [
+    "Promote Open-Source Technologies",
+    "Build a Strong Community",
+    "Organize Events and Workshops",
+    "Engage with International Open-Source Communities",
+  ];
+
   return (
     <SectionObserver id="aboutus">
       <div id="aboutus-wrapper" className="flex flex-col">
@@ -22,38 +22,34 @@ const Aboutus = () => {
             alt="Gnome Nepal Team gathering in one palce"
           />
         </div>
-        <div className="about flex flex-col gap-4 pt-8">
-          <h1 className="text-3xl flex  font-bold">Who We Are</h1>
-          <div className="text">
-            <p>
-              GNOME Nepal is an organization dedicated to promoting open-source
-              initiatives within Nepal. Our core objective is to foster a
-              vibrant community where technology enthusiasts, developers, and
-              open-source advocates can collaborate, share knowledge, and
-              contribute.
-            </p>
-          </div>
-          <h1 className="text-3xl font-bold">Our Vision</h1>
-          <div className="text">
-            <div>
-              To encourage the use of GNOME and related open-source projects in
-              Nepal and empower the local contributors to represent Nepal's Open
-              source internationally.
-            </div>
-          </div>
-          <h1 className="text-3xl font-bold">Our Mission</h1>
-          <div className="text">
-            <div>
-              <h3 className="pb-4">
-                We believe in the power of open-source to drive innovation,
-                collaboration and education. Our mission is to:
-              </h3>
-              <ul className="grid font-semibold gap-1">
-                {missions.map((mission, index) => (
-                    <li className="flex gap-2 items-center" key={index}><ChevronRight className="size-4"/> {mission}</li>
-                ))}
-              </ul>
-            </div>
+        <div className="about flex flex-col gap-4 pt-8 ">
+          <h3 className="text-2xl flex  font-bold">Who We Are</h3>
+          <p className="lg:text-lg">
+            GNOME Nepal is an organization dedicated to promoting open-source
+            initiatives within Nepal. Our core objective is to foster a vibrant
+            community where technology enthusiasts, developers, and open-source
+            advocates can collaborate, share knowledge, and contribute.
+          </p>
+          <h3 className="text-2xl font-bold">Our Vision</h3>
+          <p className="lg:text-lg">
+            To encourage the use of GNOME and related open-source projects in
+            Nepal and empower the local contributors to represent Nepal's Open
+            source internationally.
+          </p>
+
+          <h3 className="text-2xl font-bold">Our Mission</h3>
+          <div>
+            <h3 className="pb-4 lg:text-lg">
+              We believe in the power of open-source to drive innovation,
+              collaboration and education. Our mission is to:
+            </h3>
+            <ul className="grid font-semibold gap-1">
+              {missions.map((mission, index) => (
+                <li className="flex gap-2 items-center" key={index}>
+                  <ChevronRight className="size-4" /> {mission}
+                </li>
+              ))}
+            </ul>
           </div>
         </div>
       </div>
