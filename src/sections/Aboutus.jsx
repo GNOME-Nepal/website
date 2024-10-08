e1a221462cab6d7eb1cd3aa28cdf21c3784d2205
kshetritej <tejghartikshetri@gmail.com>
2024-10-08 10:07:23 +0545

    chore(about): reset style to as of initial

--- src/sections/Aboutus.jsx
+++ src/sections/Aboutus.jsx
@@ -22,7 +22,7 @@ const Aboutus = () => {
             alt="Gnome Nepal Team gathering in one palce"
           />
         </div>
-        <div className="about flex flex-col gap-4 pt-8 lg:w-[80%] ">
+        <div className="about flex flex-col gap-4 pt-8 ">
           <h3 className="text-2xl flex font-bold">Who We Are</h3>
           <p className="lg:text-lg">
             GNOME Nepal is an organization dedicated to promoting open-source
@@ -43,10 +43,10 @@ const Aboutus = () => {
               We believe in the power of open-source to drive innovation,
               collaboration and education. Our mission is to:
             </h3>
-            <ul className="grid font-semibold gap-1">
+            <ul className="list-disc grid font-semibold gap-1">
               {missions.map((mission, index) => (
-                <li className="list-disc pl-5 flex gap-2 items-center" key={index}>
-                  {mission}
+                <li className="flex gap-2 items-center" key={index}>
+                  <ChevronRight className="size-4" /> {mission}
                 </li>
               ))}
             </ul>
