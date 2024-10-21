62bb315b3a374b0189191f184877a6b022367ed8
Aaditya Thapa <81004769+Aadithapa456@users.noreply.github.com>
2024-10-21 22:56:25 +0545

    Bugfix/event section padding mobile (#41)
    * Update .gitignore
    
    * Replaced texts with icons in events section for better responsiveness
    
    * Added text-muted-foreground className in svg

--- src/components/ui/tabs.jsx
+++ src/components/ui/tabs.jsx
@@ -11,7 +11,7 @@ const TabsList = React.forwardRef(({ className, ...props }, ref) => (
   <TabsPrimitive.List
     ref={ref}
     className={cn(
-      "inline-flex h-10 items-center justify-center rounded-md bg-muted p-1 text-muted-foreground",
+      "inline-flex h-10 items-center justify-center rounded-md bg-muted px-1 text-muted-foreground",
       className
     )}
     {...props} />
