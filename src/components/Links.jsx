de82932588f83f81059498bcd202085bd5e13d1f
n1rjal <nirjalpaudel54312@gmail.com>
2024-10-23 22:06:22 +0545

    fix: css related to issue #43
    Fix the issue mentioned in issue #43

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -2,20 +2,21 @@ import React from "react";
 import { useLocation } from "react-router-dom";
 
 const Links = ({ index, to, text }) => {
-    const location = useLocation();
-    const { hash } = location;
+  const location = useLocation();
+  const { hash } = location;
 
-    const style = hash === to
-        ? "block py-2 px-3 hover:bg-gray md:hover:text-white rounded md:text-gray-900 md:p-0 md:hover:bg-black md:px-4 md:py-2 md:rounded-full md:bg-black md:text-white transition delay-80 border border-primary"
-        : "block py-2 px-3 hover:bg-gray md:hover:text-white rounded md:text-gray-900 md:p-0 md:hover:bg-black md:px-4 md:py-2 md:rounded-full transition delay-80 border border-transparent";
+  const style =
+    hash === to
+      ? "block py-2 px-3 hover:bg-gray md:hover:text-white hover:text-black rounded md:text-gray-900 md:p-0 md:hover:bg-black md:px-4 md:py-2 md:rounded-full md:bg-black md:text-white transition delay-80 border border-primary"
+      : "block py-2 px-3 hover:bg-gray md:hover:text-white hover:text-black rounded md:text-gray-900 md:p-0 md:hover:bg-black md:px-4 md:py-2 md:rounded-full transition delay-80 border border-transparent";
 
-    return (
-        <li key={index}>
-            <a href={to} className={style} aria-current="page" aria-label={text}>
-                {text}
-            </a>
-        </li>
-    );
+  return (
+    <li key={index}>
+      <a href={to} className={style} aria-current="page" aria-label={text}>
+        {text}
+      </a>
+    </li>
+  );
 };
 
 export default Links;
