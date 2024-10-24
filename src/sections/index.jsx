0e97159bb5e2c4dfc403c78621e1dbe2d3d81bd9
n1rjal <nirjalpaudel54312@gmail.com>
2024-10-24 22:40:13 +0545

    feat(worflow/contributor): workflow for contributor list

--- src/sections/index.jsx
+++
@@ -1,5 +0,0 @@
-export {default as Aboutus} from './Aboutus.jsx'
-export {default as Events} from './Events.jsx'
-export {default as Contributors} from './Contributors.jsx'
-export {default as FAQ} from './FAQ.jsx'
-export {default as Newsletter} from './Newsletter.jsx'
