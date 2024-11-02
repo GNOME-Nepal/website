1c524cc0c0e515a43349243b412394dae1bda37b
utsavdotdev <95236817+utsavdotdev@users.noreply.github.com>
2024-11-02 21:58:04 +0545

    feat: add docusarus and setup docs & blog

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,17 @@
-import path from "path"
-import react from "@vitejs/plugin-react-swc"
-import { defineConfig } from "vite"
+import path from "path";
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 
 export default defineConfig({
+  server: {
+    proxy: {
+      "/docs": "http://localhost:3000",
+    },
+  },
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
