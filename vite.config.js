d8871e3faee637beabf73a52b9cfac3556864d9a
utsavdotdev <95236817+utsavdotdev@users.noreply.github.com>
2024-11-06 12:05:56 +0545

    chore: add clean logs

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,12 @@ import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  server: {
-    proxy: {
-      "/docs": "http://localhost:3000",
-    },
-  },
+  // uncomment the following lines to enable proxy in local development
+  // server: {
+  //   proxy: {
+  //     "/docs": "http://localhost:3000",
+  //   },
+  // },
   plugins: [react()],
   resolve: {
     alias: {
