77ecc73c7cb40c14bc0006ab61f19a49d832c7d9
utsavdotdev <95236817+utsavdotdev@users.noreply.github.com>
2024-11-06 11:46:04 +0545

    chore: check commit

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,11 @@ import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  // uncomment the following lines of code in development
-  // server: {
-  //   proxy: {
-  //     "/docs": "http://localhost:3000",
-  //   },
-  // },
+  server: {
+    proxy: {
+      "/docs": "http://localhost:3000",
+    },
+  },
   plugins: [react()],
   resolve: {
     alias: {
