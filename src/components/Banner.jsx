af66379ada41428a9ff03e2c896031a228973589
Utsav bhattarai <95236817+utsavdotdev@users.noreply.github.com>
2024-12-21 22:27:56 +0545

    Optimize banner section (#90)
    * feat: enhance navbar with a dynamic upper layer  of  welcome message, improve image container in About Us section with a responsive zoom in effect
    
    * feat: add dynamic announcement layer to navbar with redirect link and icons
    
    * feat: update navbar component as per the instructions
    
    * chore(navbar): refactor code and design which align with current theme of website and optimize it
    
    ---------
    
    Co-authored-by: ryuzinoh <whysafalisop@gmail.com>
    Co-authored-by: Safal Lama <140276215+RyuZinOh@users.noreply.github.com>

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,9 +1,30 @@
-const Banner = ({ message }) => {
+import { Button } from "@/components/ui/button";
+import clsx from "clsx";
+
+const Banner = ({ data, show }) => {
+  const { emoji, message, color, button } = data;
   return (
     <>
-      <div className="hidden lg:block bg-orange-500 text-primary-foreground p-[8px] text-center text-md z-999">
-        <div className="flex items-center justify-center">
-          <p>{message}</p>
+      <div
+        className={clsx(
+          `hidden ${color} text-primary-foreground p-[8px] text-center text-md z-999 transition ease-in-out duration-300`,
+          {
+            "lg:block": show,
+          },
+        )}
+      >
+        <div className="flex items-center justify-center gap-3">
+          <p>
+            <span className="px-1 text-lg">{emoji}</span>
+            {message}
+          </p>
+          <Button
+            variant="secondary"
+            className="rounded-full text-[12px] h-7 px-3"
+            asChild
+          >
+            <a href={button.link}>{button.text}</a>
+          </Button>
         </div>
       </div>
     </>
