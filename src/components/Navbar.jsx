a116b0b8aba348574f0fb4634f569793a44a4124
utsavdotdev <95236817+utsavdotdev@users.noreply.github.com>
2024-12-21 22:18:16 +0545

    chore(navbar): refactor code and design which align with current theme of website and optimize it

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,21 @@
 import { useState, useEffect } from "react";
 import Links from "./Links";
 import { ModeToggle } from "./mode-toggle";
+import Banner from "./Banner";
 
 const Navbar = () => {
   const [open, setOpen] = useState(false);
   const [showUpperLayer, setShowUpperLayer] = useState(true);
-  const [isMobile, setIsMobile] = useState(false);
-
-  // Announcement text and registration link
-  const announcementText =
-    "We are organizing UbuCon 2025! Volunteer registration is now open. ";
-  const applyLink = "registrationformSITEurllink";
+  const banner_data = {
+    emoji: "🚀",
+    message:
+      "We are organizing UbuCon 2025! Volunteer registration is now open.",
+    color: "bg-blue-400",
+    button: {
+      text: "Apply Here",
+      link: "https://docs.google.com/forms/d/e/1FAIpQLSdgYpJJjErK9W5Pjw4GmMStXUcrmngfPteR_BpvY5awxzj5aQ/viewform",
+    },
+  };
 
   const toggleMenu = () => {
     setOpen(!open);
@@ -35,101 +40,18 @@ const Navbar = () => {
     };
 
     window.addEventListener("scroll", handleScroll);
-
-    // detects mobile screen size
-    const checkIfMobile = () => {
-      setIsMobile(window.innerWidth < 1024);
-    };
-
-    checkIfMobile();
-    window.addEventListener("resize", checkIfMobile);
-
     return () => {
       window.removeEventListener("scroll", handleScroll);
-      window.removeEventListener("resize", checkIfMobile);
     };
   }, []);
 
   return (
     <>
-      {/* Upper Layer for Desktop */}
-      {!isMobile && (
-        <div
-          className={`fixed w-full z-50 top-0 left-0 py-7 px-4  transition-transform duration-300 ${showUpperLayer ? "translate-y-0" : "-translate-y-full"}`}
-          style={{ backgroundColor: "cyan", color: "black" }}
-        >
-          <div className="max-w-screen-xl mx-auto relative flex items-center">
-            {/* Left Side: Logo*/}
-            <a
-              href="/"
-              className="absolute left-20 flex items-center space-x-2 pl-4"
-            >
-              <img
-                src={"images/GNOMENepal.png"}
-                alt="Gnome Nepal Logo"
-                height="60px"
-                width="250px"
-              />
-            </a>
-
-            {/* Center: Announcement*/}
-            <div className="flex-1 text-center px-16">
-              <span className="text-lg font-medium">{announcementText}</span>{" "}
-            </div>
-
-            {/* Right Side: Apply Here Button */}
-            <div className="absolute right-40 pr-10 text-sm mt-1">
-              <a
-                href={applyLink}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="inline-block px-4 py-1.5 bg-black text-white font-medium rounded-full border border-black hover:bg-white hover:text-black transition"
-              >
-                Apply Here
-              </a>
-            </div>
-          </div>
-        </div>
-      )}
-
-      {/* Responsive Upper Layer for Mobile */}
-      {isMobile && (
-        <div
-          className={`fixed w-full z-50 top-0 left-0 py-3 px-3 transition-transform duration-300 ${showUpperLayer ? "translate-y-0" : "-translate-y-full"} lg:hidden`}
-          style={{ backgroundColor: "cyan", color: "black" }}
-        >
-          <div className="max-w-screen-xl mx-auto relative flex items-center justify-between">
-            {/* Left Side: Logo */}
-            <a href="/" className="flex items-center space-x-2 pl-3">
-              <img
-                src={"images/GNOMENepal.png"}
-                alt="Gnome Nepal Logo"
-                width="280px"
-              />
-            </a>
-
-            {/* Right Side: Announcement Text and Apply Here Button */}
-            <div className="flex flex-col items-end pr-3">
-              <span className="text-xs font-medium text-center">
-                {announcementText}
-              </span>{" "}
-              <a
-                href={applyLink}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="mt-1 inline-block px-3 py-1 text-xs bg-black text-white font-medium rounded-full border border-black hover:bg-white hover:text-black transition"
-              >
-                Apply Here
-              </a>
-            </div>
-          </div>
-        </div>
-      )}
-
       {/* Main Navbar */}
       <nav
-        className={`fixed w-full z-40 left-0 border-b border-gray-300 bg-background dark:bg-gray-900 transition-[top] duration-300 ${showUpperLayer ? "top-[80px]" : "top-0"}`}
+        className={`fixed w-full z-40 left-0 border-b border-gray-300 bg-background dark:bg-gray-900 transition-[top] duration-300`}
       >
+        <Banner data={banner_data} show={showUpperLayer} />
         <div className="px-4 sm:px-6 lg:px-8">
           <div className="max-w-screen-xl mx-auto flex items-center justify-between py-3">
             {/* Logo Section */}
